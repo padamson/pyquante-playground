a842ff737d4e072842edf963ab6a264298d03ac6
gracetangg <gracehtang@yahoo.com>
2021-07-14 15:26:08 -0400

    remove edits to pyquante2-testing

--- pyquante2-testing.py
+++ pyquante2-testing.py
@@ -8,18 +8,3 @@ print(pyquante2.h2o)
 str(pyquante2.h2o)
 # %%
 repr(pyquante2.h2o)
-# %%
-from pyquante2 import molecule,rhf,uhf,rohf,h2,h2o,lih,basisset
-
-bfs = basisset(h2,'sto3g')
-bfs
-# %%
-solver = rhf(h2,bfs)
-solver
-
-# z axis in chart is the average distance in bohrs (from angstroms)
-# %%
-ens = solver.converge()
-ens
-
-# %%
